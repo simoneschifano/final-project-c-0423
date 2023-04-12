df2852b70b8378640d6d75310c51e0df2eea4af4
EricaCandido <ericandido1997@gmail.com>
2023-04-12 17:32:25 +0200

    12-04-2023

--- movieplex/src/components/footer/Footer.jsx
+++ movieplex/src/components/footer/Footer.jsx
@@ -1,11 +1,19 @@
 import styles from "./index.module.scss";
-
+import logo from "../../assets/logo.png";
 const Footer = () => {
-	return (
-		<div className={styles.Footer}>
-			<p>aaaa</p>
-		</div>
-	);
+  return (
+    <div className={styles.Footer}>
+      <img className={styles.logo} src={logo} alt="logo" />
+      <div className={styles.text}>
+        <p>Contattaci</p>
+        <p>Segnala un bug</p>
+        <p>Tutti i nostri cinema</p>
+      </div>
+      <div className={styles.rights}>
+        <p>MoviePlex® 2023 tutti i diritti riservati </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
