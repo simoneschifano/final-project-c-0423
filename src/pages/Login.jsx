cb4af9c6a30b5841e6d1f9fe2db1fc131536c982
EricaCandido <ericandido1997@gmail.com>
2023-04-21 16:37:56 +0200

    21-04-2023

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ import { initRegisteredUsers } from "../store/state";
 import { mainReducer } from "../store/reducer";
 import { useNavigate } from "react-router-dom";
 import ModalError from "../components/modalError";
+import image from "../assets/image.png";
 
 const Login = () => {
   const [state, dispatch] = useReducer(mainReducer, initRegisteredUsers);
@@ -21,39 +22,38 @@ const Login = () => {
       (user) => user.password === password && user.username === username
     )
       ? (localStorage.setItem("auth", username), navigate(`/?user=${username}`))
-      : (setModalstate(true),
-        // alert("La password o la mail non è corretta"),
-        setPassword(""),
-        setUsername(""));
+      : (setModalstate(true), setPassword(""), setUsername(""));
   };
   return (
     <div className={styles.LoginPage}>
-      {console.log(state.users)}
-      <form className={styles.loginForm} onSubmit={onHandleSubmit}>
-        <h3>Login</h3>
-        <input
-          className={styles.textInput}
-          type="text"
-          placeholder="Nome Utente"
-          value={username}
-          onChange={onHandleUserInput}
-        />
-        <input
-          className={styles.textInput}
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={onHandlePassInput}
-        />
-        <p>Recupera la password</p>
-        <input type="submit" value="Entra" className={styles.Button} />
-      </form>
-      <div className={styles.registerSection}>
-        <p>Non sei ancora registrato?</p>
-        <button className={styles.Button}>
-          <Link to={"/register"}>Registrati</Link>
-        </button>
+      <div className={styles.forms}>
+        <form className={styles.loginForm} onSubmit={onHandleSubmit}>
+          <h3>Login</h3>
+          <input
+            className={styles.textInput}
+            type="text"
+            placeholder="Nome Utente"
+            value={username}
+            onChange={onHandleUserInput}
+          />
+          <input
+            className={styles.textInput}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={onHandlePassInput}
+          />
+          <p>Recupera la password</p>
+          <input type="submit" value="Entra" className={styles.Button} />
+        </form>
+        <div className={styles.registerSection}>
+          <p>Non sei ancora registrato?</p>
+          <button className={styles.Button}>
+            <Link to={"/register"}>Registrati</Link>
+          </button>
+        </div>
       </div>
+      <img className={styles.backgroundImage} src={image} alt="image" />
       {modalState && (
         <ModalError
           setCheckState={setModalstate}
